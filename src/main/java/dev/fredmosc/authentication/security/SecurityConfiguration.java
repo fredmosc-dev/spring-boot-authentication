bb38428356e5ef31be9fb7495a464839fa7835f8
Frederick Moschkowich <fm.webdeveloper@gmail.com>
2020-06-10 15:49:22 -0300

    Enabled PrePostEnabled for authorization based on the roles

--- src/main/java/dev/fredmosc/authentication/security/SecurityConfiguration.java
+++ src/main/java/dev/fredmosc/authentication/security/SecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,7 +23,7 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 
 @EnableWebSecurity
 @Configuration
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private final Logger logger = LoggerFactory.getLogger(SecurityConfiguration.class);
